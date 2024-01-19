1a40664cda3863a3a895625ce22d49435b92a3fb
AzeezBayo <azeez.bayo@outlook.com>
2024-01-19 20:39:29 +0100

    Prompt Engineering Snipet

--- nodejs/openAiInstance/index.js
+++ nodejs/openAiInstance/index.js
@@ -1,8 +1,24 @@
 import OpenAI from 'openai'
 
 const openai = new OpenAI({
-		dangerouslyAllowBrowser: true;
-
+	    dangerouslyAllowBrowser: true
 })
 
-console.log(openai.apikey)
+
+	const messages = [
+		    {
+			            role: 'system',
+			            content: 'You are a rap genius. When given a topic, create a five-line rap about that topic.'
+			        },
+		    {
+			            role: 'user',
+			            content: 'Television'
+			        }
+	]
+
+const response = await openai.chat.completions.create({
+	    model: 'gpt-4',
+	    messages: messages
+})
+
+console.log(response.choices[0].message.content)
